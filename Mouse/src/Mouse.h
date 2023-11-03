560c5af821e27853331b00a40ecd5baec19102fb
3nginius <135216042+3nginius@users.noreply.github.com>
2023-11-03 11:29:23 +0300

    Updated definitions

--- Mouse/src/Mouse.h
+++ Mouse/src/Mouse.h
@@ -1,44 +1,25 @@
-/*
-  Mouse.h
-
-  Copyright (c) 2015, Arduino LLC
-  Original code (pre-library): Copyright (c) 2011, Peter Barrett
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
 #ifndef MOUSE_h
 #define MOUSE_h
-
+ 
 #include "HID.h"
-
+ 
 #if !defined(_USING_HID)
-
+ 
 #warning "Using legacy HID core (non pluggable)"
-
+ 
 #else
-
+ 
 //================================================================================
 //================================================================================
 //  Mouse
-
+ 
 #define MOUSE_LEFT 1
 #define MOUSE_RIGHT 2
 #define MOUSE_MIDDLE 4
+#define MOUSE_XB1 8
+#define MOUSE_XB2 16
 #define MOUSE_ALL (MOUSE_LEFT | MOUSE_RIGHT | MOUSE_MIDDLE)
-
+ 
 class Mouse_
 {
 private:
@@ -55,6 +36,6 @@ public:
   bool isPressed(uint8_t b = MOUSE_LEFT); // check LEFT by default
 };
 extern Mouse_ Mouse;
-
+ 
 #endif
 #endif
